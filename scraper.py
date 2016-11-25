319fc091ae6873b1442a55212128912b15603c1e
Shemamforash <samwest2@hotmail.co.uk>
2016-11-25 22:05:20 +0000

    now takes mature deviations

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 from lxml import html
 import requests
-import urllib.request
+import urllib2
 import os
 import sys
 import random
+from robobrowser import RoboBrowser
+
 from multiprocessing.dummy import Pool as ThreadPool
 
 artist_name = ''
@@ -11,37 +13,35 @@ artist_name = ''
 
 # Downloads a single image given an image page and a directory to save it
 def download_img(deviation_page, directory_to_save):
-    if deviation_page.status_code == requests.codes.ok:
         # Grab all images within a 'view-deviation' div, since these img elements are on the gallery page
-        image_urls = html.fromstring(deviation_page.content).xpath('//div[@class="dev-view-deviation"]/img/@src')
-        filename = ''
-        # If image_urls has length 2 then we have scraped a low and high quality image, so take the high quality one
-        if len(image_urls) == 2:
-            filename = image_urls[1].split('/')[-1]
-        elif len(image_urls) == 1:
-            filename = image_urls[0].split('/')[-1]
-        # If a valid url was available from the img src (image_urls had length greater than 0) then just open the page
-        # and grab the image, before saving it to the desktop. NOTE if a file with this name already exists it will
-        # be overwritten.
-        if filename != '':
-            file = urllib.request.urlopen(image_urls[1])
-            # Get rid of the artist's name
-            img_name = filename.split('_by_')[0]
-            # Extract the file ending as well
-            file_ending = filename.split('.')[-1]
-            # Piece it back together without the artist's name
-            fullname = directory_to_save + img_name + '.' + file_ending
-            # And write it to the directory
-            if os.path.isfile(fullname):
-                fullname = fullname.split('.')[0] + str(random.randrange(0, 100000)) + '.' + file_ending
-            try:
-                out = open(fullname, "wb")
-                out.write(file.read())
-                out.close()
-                return True
-            except OSError:
-                return False
-    return False
+    image_urls = html.fromstring(deviation_page).xpath('//div[@class="dev-view-deviation"]/img/@src')
+    filename = ''
+    # If image_urls has length 2 then we have scraped a low and high quality image, so take the high quality one
+    if len(image_urls) == 2:
+        filename = image_urls[1].split('/')[-1]
+    elif len(image_urls) == 1:
+        filename = image_urls[0].split('/')[-1]
+    # If a valid url was available from the img src (image_urls had length greater than 0) then just open the page
+    # and grab the image, before saving it to the desktop. NOTE if a file with this name already exists it will
+    # be overwritten.
+    if filename != '':
+        image_file = urllib2.urlopen(image_urls[1])
+        # Get rid of the artist's name
+        img_name = filename.split('_by_')[0]
+        # Extract the file ending as well
+        file_ending = filename.split('.')[-1]
+        # Piece it back together without the artist's name
+        fullname = directory_to_save + img_name + '.' + file_ending
+        # And write it to the directory
+        if os.path.isfile(fullname):
+            fullname = fullname.split('.')[0] + str(random.randrange(0, 100000)) + '.' + file_ending
+        try:
+            out = open(fullname, "wb")
+            out.write(image_file.read())
+            out.close()
+            return True
+        except OSError:
+            return False
 
 
 # Returns all valid urls from a gallery page that link to a deviation page
@@ -77,11 +77,31 @@ def scrape_this_page(page_name):
         pictures = get_image_urls(
             html.fromstring(page.content).xpath('//a[not(ancestor::div[@class="gr-body"])]/@href'))
         downloaded_images = []
+        s = requests.session()
+        s.headers.update({'Referer': 'http://www.deviantart.com/'})
+        USERAGENTS = (
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:7.0.1) Gecko/20100101',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)',
+            'Opera/9.99 (Windows NT 5.1; U; pl) Presto/9.9.9',
+            'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-US) AppleWebKit/530.5 (KHTML, like Gecko) Chrome/ Safari/530.5',
+            'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/6.0',
+            'Mozilla/5.0 (Windows; U; Windows NT 6.1; pl; rv:1.9.1) Gecko/20090624 Firefox/3.5 (.NET CLR 3.5.30729)'
+        )
+        browser = RoboBrowser(history=False, session=s, tries=3, user_agent=random.choice(USERAGENTS))
+        browser.open(
+            'https://www.deviantart.com/users/login?ref=http%3A%2F%2Fwww.deviantart.com%2F&remember_me=1')
+        form = browser.get_forms()[1]
+        form['username'] = 'Mesmer12345'
+        form['password'] = 'OROsirian#629'
+        browser.submit_form(form)
         for picture in pictures:
             # Make sure image has not already been downloaded, and that it is not simply a duplicate url with
             # the comments section open
             if picture not in downloaded_images and '#comments' not in picture:
-                deviation_page = requests.get(picture)
+                browser.open(picture)
+                deviation_page = str(browser.parsed)
                 if download_img(deviation_page, get_folder(page_name)):
                     print("Grabbed " + picture)
                     downloaded_images.append(picture)
